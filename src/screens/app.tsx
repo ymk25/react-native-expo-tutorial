ca97e5b0406810b28f6c3f0cf8d8849ca79a2b56
ymk25 <ymk.gr.22@gmail.com>
2025-07-14 20:07:56 +0900

    chore: husky + lint-stagedを追加

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -6,7 +6,7 @@ import { StatusBar } from "expo-status-bar";
 const App: FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up app.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
