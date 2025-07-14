1ee18942627eaca686dc4a9c75a987adcb9fc760
ymk25 <ymk.gr.22@gmail.com>
2025-07-14 20:42:44 +0900

    chore: lint実行エラー対応

--- eslint.config.js
+++ eslint.config.js
@@ -6,80 +6,70 @@ import expoConfig from "eslint-config-expo/flat.js";
 import { defineConfig } from "eslint/config";
 
 // https://docs.expo.dev/guides/using-eslint/
-export default defineConfig([
-  // Base recommended config
-  js.configs.recommended,
+export default defineConfig({
+  // Ignore patterns
+  ignores: [
+    "babel.config.js",
+    // Add other files to ignore as needed
+    "node_modules/**",
+    "dist/**",
+    "build/**",
+    ".expo/**",
+    "ios/**",
+    "android/**",
+  ],
 
-  // Expo config
-  expoConfig,
+  // Base recommended config
+  extends: [js.configs.recommended, ...expoConfig],
 
   // TypeScript configuration
-  {
-    files: ["**/*.{ts,tsx,js,jsx}"],
-    languageOptions: {
-      parser: typescriptParser,
-      parserOptions: {
-        project: true,
-        ecmaFeatures: {
-          jsx: true,
-        },
+  files: ["**/*.{ts,tsx,js,jsx}"],
+  languageOptions: {
+    parser: typescriptParser,
+    parserOptions: {
+      project: true,
+      ecmaFeatures: {
+        jsx: true,
       },
     },
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-    },
-    rules: {
-      // TypeScript ESLint recommended rules
-      ...typescriptEslint.configs.recommended.rules,
-      ...typescriptEslint.configs["recommended-type-checked"].rules,
-      ...typescriptEslint.configs["stylistic-type-checked"].rules,
-
-      // Custom rules
-      // @see https://typescript-eslint.io/rules/array-type
-      "@typescript-eslint/array-type": "off",
-      // @see https://typescript-eslint.io/rules/consistent-type-definitions/
-      "@typescript-eslint/consistent-type-definitions": "off",
-      // @see https://typescript-eslint.io/rules/consistent-type-imports/
-      "@typescript-eslint/consistent-type-imports": [
-        "warn",
-        {
-          prefer: "type-imports",
-          fixStyle: "inline-type-imports",
-        },
-      ],
-      // @see https://typescript-eslint.io/rules/no-unused-vars/
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
-      ],
-      // @see https://typescript-eslint.io/rules/require-await/
-      "@typescript-eslint/require-await": "off",
-      // @see https://typescript-eslint.io/rules/no-misused-promises/
-      "@typescript-eslint/no-misused-promises": [
-        "error",
-        {
-          checksVoidReturn: { attributes: false },
-        },
-      ],
-      // https://typescript-eslint.io/rules/no-var-requires/
-      "@typescript-eslint/no-var-requires": "off",
-      // Disable unsafe rules
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-argument": "off",
-    },
   },
+  plugins: {
+    "@typescript-eslint": typescriptEslint,
+  },
+  rules: {
+    // TypeScript ESLint recommended rules
+    ...typescriptEslint.configs.recommended.rules,
+    ...typescriptEslint.configs["recommended-type-checked"].rules,
+    ...typescriptEslint.configs["stylistic-type-checked"].rules,
 
-  // Ignore patterns
-  {
-    ignores: [
-      "babel.config.js",
-      // Add other files to ignore as needed
-      "node_modules/**",
-      "dist/**",
-      "build/**",
-      ".expo/**",
-      "ios/**",
-      "android/**",
+    // Custom rules
+    // @see https://typescript-eslint.io/rules/array-type
+    "@typescript-eslint/array-type": "off",
+    // @see https://typescript-eslint.io/rules/consistent-type-definitions/
+    "@typescript-eslint/consistent-type-definitions": "off",
+    // @see https://typescript-eslint.io/rules/consistent-type-imports/
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
     ],
+    // @see https://typescript-eslint.io/rules/no-unused-vars/
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    // @see https://typescript-eslint.io/rules/require-await/
+    "@typescript-eslint/require-await": "off",
+    // @see https://typescript-eslint.io/rules/no-misused-promises/
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
+    // https://typescript-eslint.io/rules/no-var-requires/
+    "@typescript-eslint/no-var-requires": "off",
+    // Disable unsafe rules
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
   },
-]);
+});
