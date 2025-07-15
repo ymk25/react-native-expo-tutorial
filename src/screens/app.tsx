8a7a2e4e200e376b7752810c363d100b61772299
ymk25 <ymk.gr.22@gmail.com>
2025-07-16 01:11:20 +0900

    feat: 絵文字ピッカーを表示するボタンを追加

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -5,6 +5,8 @@ import * as ImagePicker from "expo-image-picker";
 import { StatusBar } from "expo-status-bar";
 
 import { Button } from "@/components/button";
+import { CircleButton } from "@/components/circle-button";
+import { IconButton } from "@/components/icon-button";
 import { ImageViewer } from "@/components/image-viewer";
 
 const PlaceholderImage =
@@ -13,6 +15,18 @@ const PlaceholderImage =
 const App: FC = () => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [showAppOptions, setShowAppOptions] = useState(false);
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    // TODO: あとで
+  };
+
+  const onSaveImageAsync = async () => {
+    // TODO: あとで
+  };
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -38,7 +52,17 @@ const App: FC = () => {
         />
       </View>
       {showAppOptions ? (
-        <View />
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="リセット" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="保存"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
       ) : (
         <View style={styles.footerContainer}>
           <Button theme="primary" label="写真を選択" onPress={pickImageAsync} />
@@ -66,6 +90,14 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
 
