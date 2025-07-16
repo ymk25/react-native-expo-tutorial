29fde847da5471df77aa6eb852dad36528025250
ymk25 <ymk.gr.22@gmail.com>
2025-07-17 00:27:30 +0900

    feat: React Native Gesture Handler を設定

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -1,5 +1,6 @@
 import { useState, type FC } from "react";
 import { StyleSheet, View, type ImageSourcePropType } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
 import * as ImagePicker from "expo-image-picker";
 import { StatusBar } from "expo-status-bar";
@@ -55,7 +56,7 @@ const App: FC = () => {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           placeholderImageSource={PlaceholderImage}
@@ -90,7 +91,7 @@ const App: FC = () => {
         </View>
       )}
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 };
 
