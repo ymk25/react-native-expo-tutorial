f73db11a40ed9e81918db7e56e54b124c9ecf96d
ymk25 <ymk.gr.22@gmail.com>
2025-07-17 01:22:11 +0900

    feat: プラットフォーム固有のコードを追加

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -1,11 +1,17 @@
 import { useRef, useState, type FC } from "react";
-import { StyleSheet, View, type ImageSourcePropType } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  type ImageSourcePropType,
+} from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { captureRef } from "react-native-view-shot";
 import { registerRootComponent } from "expo";
 import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 import { StatusBar } from "expo-status-bar";
+import domtoimage from "dom-to-image";
 
 import { Button } from "@/components/button";
 import { CircleButton } from "@/components/circle-button";
@@ -26,7 +32,7 @@ const App: FC = () => {
     null,
   );
   const [status, requestPermission] = MediaLibrary.usePermissions();
-  const imageRef = useRef<View>(null);
+  const imageRef = useRef(null);
 
   if (status === null) {
     void requestPermission();
@@ -45,18 +51,35 @@ const App: FC = () => {
   };
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
+    if (Platform.OS !== "web") {
+      if (imageRef.current) {
+        try {
+          const localUri = await captureRef(imageRef, {
+            height: 440,
+            quality: 1,
+          });
 
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert("保存しました");
+          await MediaLibrary.saveToLibraryAsync(localUri);
+          if (localUri) {
+            alert("保存しました");
+          }
+        } catch (e) {
+          console.log(e);
+        }
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      if (imageRef.current) {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        const link = document.createElement("a");
+        link.download = "sticker-image.jpeg";
+        link.href = dataUrl;
+        link.click();
+      }
     }
   };
 
