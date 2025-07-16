0158637ea6454a228c36ff6a18d8e33296c08903
ymk25 <ymk.gr.22@gmail.com>
2025-07-17 00:57:38 +0900

    feat: パンジェスチャーを追加

--- src/components/emoji-sticker.tsx
+++ src/components/emoji-sticker.tsx
@@ -1,5 +1,5 @@
 import { type FC } from "react";
-import { View, type ImageSourcePropType } from "react-native";
+import { type ImageSourcePropType } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
   useAnimatedStyle,
@@ -14,6 +14,22 @@ type EmojiStickerProps = {
 
 export const EmojiSticker: FC<EmojiStickerProps> = (props) => {
   const scaleImage = useSharedValue(props.imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const drag = Gesture.Pan().onChange((event) => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    };
+  });
 
   const imageStyle = useAnimatedStyle(() => {
     return {
@@ -31,17 +47,19 @@ export const EmojiSticker: FC<EmojiStickerProps> = (props) => {
     });
 
   return (
-    <View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image
-          source={props.stickerSource}
-          resizeMode="contain"
-          style={[
-            imageStyle,
-            { width: props.imageSize, height: props.imageSize },
-          ]}
-        />
-      </GestureDetector>
-    </View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={{ top: -350 }}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={props.stickerSource}
+            resizeMode="contain"
+            style={[
+              imageStyle,
+              { width: props.imageSize, height: props.imageSize },
+            ]}
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 };
